466d6688a8a9f6b71b2cf3cbc47a47c3596b047f
nateomach1-debug <nateomach1@gmail.com>
2026-09-22 19:51:49 -0600

    Integrate joystick for touch movement controls
    Added joystick control for touch movement and updated player reference handling.

--- scripts/ui/touch_controls.gd
+++ scripts/ui/touch_controls.gd
@@ -1,26 +1,29 @@
 extends CanvasLayer
 
-@onready var btn_up: Button = $DPad/Up
-@onready var btn_down: Button = $DPad/Down
-@onready var btn_left: Button = $DPad/Left
-@onready var btn_right: Button = $DPad/Right
+@onready var joystick: Control = $Joystick
 @onready var btn_jump: Button = $Actions/Jump
 @onready var btn_sprint: Button = $Actions/Sprint
 @onready var btn_shoot: Button = $Actions/Shoot
 @onready var btn_reload: Button = $Actions/Reload
 @onready var btn_interact: Button = $Interact
 
+var _player: Node
+
 
 func _ready() -> void:
-	_bind(btn_up, "move_forward")
-	_bind(btn_down, "move_back")
-	_bind(btn_left, "move_left")
-	_bind(btn_right, "move_right")
 	_bind(btn_jump, "jump")
 	_bind(btn_sprint, "sprint")
 	_bind(btn_shoot, "shoot")
 	_bind(btn_reload, "reload")
 	_bind(btn_interact, "interact")
+	_player = get_tree().get_first_node_in_group("player")
+
+
+func _process(_delta: float) -> void:
+	if not _player:
+		_player = get_tree().get_first_node_in_group("player")
+		return
+	_player.touch_move_vector = joystick.value
 
 
 func _bind(button: Button, action: String) -> void:
